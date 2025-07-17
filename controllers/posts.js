3f1fa3c7d1139e642fe78d1fe825867082218053
tommaso-mandorino <tommasomandorino@outlook.it>
2025-07-18 00:33:36 +0200

    feat: finish milestone 3

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,42 @@
 // Database connection importing
 const connection = require('../database/connection.js');
 
+// Internal server error function declaration
+function internalServerError(routeResponseObject) {
+
+    // Create an error response object
+    const errorResponseObject = {
+        error: true,
+        message: 'Internal server error.'
+    };
+
+    // Set response status code to 500 (internal server error)
+    routeResponseObject.status(500);
+
+    // Send error response
+    routeResponseObject.json(errorResponseObject);
+
+}
+
+// Not found function declaration
+function notFoundError(notFoundId, routeResponseObject) {
+ 
+    // Create an error response object
+    const errorResponseObject =  {
+        error: true,
+        errorCode: 404,
+        errorString: 'Not found',
+        message: `Post with ID ${notFoundId} not found.`
+    };
+
+    // Set response status code to 404 (not found)
+    routeResponseObject.status(404);
+
+    // Send error response
+    routeResponseObject.json(errorResponseObject);
+
+}
+
 // Index
 function index(request, response) {
 
@@ -11,17 +47,8 @@ function index(request, response) {
         // IF there is a query error
         if(error) {
 
-            // Create an error response object
-            const errorResponseObject = {
-                error: true,
-                message: 'Internal server error.'
-            };
-
-            // Set response status code to 500 (internal server error)
-            response.status(500);
-
-            // Send error response
-            response.json(errorResponseObject);
+            // Call internal server error function
+            internalServerError(response);
 
             // Stop function execution
             return;
@@ -38,7 +65,39 @@ function index(request, response) {
 // Show
 function show(request, response) {
 
+    // Get request id parameter
+    const requestId = request.params.id;
 
+    const showQuery = 'SELECT * FROM `posts` WHERE `id` = ?;';
+
+    connection.query(showQuery, [requestId], (error, result) => {
+
+        // IF there is a query error
+        if (error) {
+
+            // Call internal server error function
+            internalServerError(response);
+
+            // Stop function execution
+            return;
+
+        }
+
+        // IF requested ID doesn't exist
+        if (result.length === 0) {
+
+            // Call not found error (404)
+            notFoundError(requestId, response);
+
+            // Stop function execution
+            return;
+
+        }
+
+        // Send query results
+        response.send(result[0]);
+
+    });
 
 }
 
